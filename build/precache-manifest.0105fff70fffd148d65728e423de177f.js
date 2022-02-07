a1e4b6f7091f622bb13186e93a5645f62fa7a616
Soumali Gorai <66300439+soumali28@users.noreply.github.com>
2022-02-07 21:03:22 +0530

    Add files via upload

---
+++ build/precache-manifest.0105fff70fffd148d65728e423de177f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9085d1578d261b32da6fa44f6ccc691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44d3e9d7d528e08f604e",
+    "url": "/static/js/2.8f6f9f51.chunk.js"
+  },
+  {
+    "revision": "1741b06dda1350f8e41a",
+    "url": "/static/js/main.c7acb316.chunk.js"
+  },
+  {
+    "revision": "7a6542f573e376255b84",
+    "url": "/static/js/runtime-main.7bab651c.js"
+  }
+]);
